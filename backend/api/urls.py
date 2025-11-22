d2bde1dc4c9c0463737b8ac99de20285334611ee
n1ckg4m3s <nico.ag.03@hotmail.com>
2025-11-22 16:15:36 -0300

    [ FEAT ] Adicionando nome as rotas

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PostMessageView, GetHistoryView
 
 urlpatterns = [
-    path("messages/", PostMessageView.as_view()),
-    path("messages/history/", GetHistoryView.as_view()),
+    path("messages/", PostMessageView.as_view(), name="sendMessages"),
+    path("messages/history/", GetHistoryView.as_view(), name="getHistory"),
 ]
